57c9461cf06422d094047b07f948a378d71a17b5
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-06 01:25:26 +0300

    Add files via upload

--- Client/client.py
+++ Client/client.py
@@ -1,10 +1,15 @@
 import getpass
+import os.path
 from datetime import datetime
 import socketio
 import socket
 import psutil
 from threading import Thread
 import time
+import win32con
+import win32gui
+import win32ui
+import requests
 
 sio = socketio.Client()
 
@@ -32,10 +37,71 @@ def on_connect():
                              })
 
 
+@sio.on('message')
+def handle_message(message):
+    print('Received message: ' + message)
+
+
+def get_screenshot():
+    client_hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(client_hostname)
+
+    file_path = r'c:/HandsOff'
+    file_name = f'screenshot {ip_address} {get_date()}.jpg'
+    filename = os.path.join(file_path, file_name)
+
+    desktop = win32gui.GetDesktopWindow()
+
+    SM_CXVIRTUALSCREEN = 78
+    SM_CYVIRTUALSCREEN = 79
+    SM_XVIRTUALSCREEN = 76
+    SM_YVIRTUALSCREEN = 77
+
+    width = ctypes.windll.user32.GetSystemMetrics(SM_CXVIRTUALSCREEN)
+    height = ctypes.windll.user32.GetSystemMetrics(SM_CYVIRTUALSCREEN)
+    left = ctypes.windll.user32.GetSystemMetrics(SM_XVIRTUALSCREEN)
+    top = ctypes.windll.user32.GetSystemMetrics(SM_YVIRTUALSCREEN)
+
+    desktop_dc = win32gui.GetWindowDC(desktop)
+    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+
+    mem_dc = img_dc.CreateCompatibleDC()
+    screenshot = win32ui.CreateBitmap()
+    screenshot.CreateCompatibleBitmap(img_dc, width, height)
+    mem_dc.SelectObject(screenshot)
+    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
+    screenshot.SaveBitmapFile(mem_dc, filename)
+    mem_dc.DeleteDC()
+    win32gui.DeleteObject(screenshot.GetHandle())
+
+    with open(filename, 'rb') as f:
+        file_data = f.read()
+
+    headers = {
+        'Content-Type': 'multipart/form-data',
+    }
+
+    data = {
+        'file': (filename, file_data, 'image/jpeg'),
+        'file_name': file_name,
+        'file_size': len(file_data),
+        'file_type': 'image/jpeg'
+    }
+
+    response = requests.post('http://localhost:5000/receive-file', headers=headers, data=data)
+    print(response.text)
+
+
+def get_date() -> str:
+    d = datetime.now().replace(microsecond=0)
+    dt = str(d.strftime("%d.%b.%y %H.%M.%S"))
+    return dt
+
+
 def keep_alive():
-    while True:
-        sio.send('heartbeat')
-        time.sleep(10)
+    sio.send('heartbeat')
+    time.sleep(10)
 
 
 # Wait for events
