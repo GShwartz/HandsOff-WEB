996f907c146de3651e3442c226997387365512e8
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-12 23:20:58 +0300

    Add files via upload

--- static/controller.js
+++ static/controller.js
@@ -1,17 +1,32 @@
-  const buttonsContainer = document.querySelector('.buttons-container');
-  buttonsContainer.addEventListener('click', handleButtonClick);
+const buttonsContainer = document.querySelector('.buttons-container');
+buttonsContainer.addEventListener('click', handleButtonClick);
 
-  function handleButtonClick(event) {
+function handleButtonClick(event) {
     const button = event.target.closest('.button');
     if (!button) return;
 
     const action = button.dataset.action;
-    makeAjaxRequest(action);
-  }
+    if (action === 'screenshot') {
+        makeAjaxRequest(action);
+        refreshImageSlider();
+  } else {
+        makeAjaxRequest(action);
+    }
+}
 
-  function makeAjaxRequest(data) {
+function makeAjaxRequest(data) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/controller');
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
     xhr.send(JSON.stringify({ data }));
-  }
+}
+
+function refreshImageSlider() {
+    const row = document.querySelector('table tr.row-data');
+    const clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    row.dispatchEvent(clickEvent);
+};
