41e8eb3bee2621e73e1503b6f0a3fd3408498baf
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-14 23:32:21 +0300

    Add files via upload

--- static/minimizer.js
+++ static/minimizer.js
@@ -1,3 +1,15 @@
+const tableContainer = document.querySelector('.history-table-container');
+const minimizeBtn = document.querySelector('.history-table-minimize-button');
+const table = tableContainer.querySelector('table');
+
+minimizeBtn.addEventListener('click', () => {
+  tableContainer.classList.toggle('minimized');
+});
+
+table.addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+
 const sliderWrapper = document.querySelector('.slider-wrapper');
 const minimizeButton = document.querySelector('.minimize-button');
 
