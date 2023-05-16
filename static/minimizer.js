11b444341c6b15ccb841d30f103ddbd5ca43ecf8
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-16 19:05:34 +0300

    Fixed bug where not minimizing @ restart

--- static/minimizer.js
+++ static/minimizer.js
@@ -1,43 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const tableContainer = document.querySelector('.history-table-container');
-  const minimizeBtn = document.querySelector('.history-table-minimize-button');
-  const table = tableContainer.querySelector('table');
-  const sliderWrapper = document.querySelector('.slider-wrapper');
-  const minimizeButton = sliderWrapper.querySelector('.screenshots-min-button');
-  const slider = sliderWrapper.querySelector('.slider');
+    const tableContainer = document.querySelector('.history-table-container');
+    const minimizeBtn = document.querySelector('.history-table-minimize-button');
+    const table = tableContainer.querySelector('table');
+    const sliderWrapper = document.querySelector('.slider-wrapper');
+    const minimizeButton = sliderWrapper.querySelector('.screenshots-min-button');
+    const slider = sliderWrapper.querySelector('.slider');
 
-  minimizeBtn.addEventListener('click', () => {
-    tableContainer.classList.toggle('minimized');
-    if (tableContainer.classList.contains('minimized')) {
-      minimizeBtn.textContent = '+';
-    } else {
-      minimizeBtn.textContent = '-';
+    document.addEventListener('stationValue', (event) => {
+        if (station === false) {
+            const minimizeBtn = document.querySelector('.history-table-minimize-button');
+            const minimizeButton = document.querySelector('.screenshots-min-button');
+            minimizeBtn.click();
+            minimizeButton.click();
+        }
+    });
+
+    // Function to check if any row is selected
+    function isRowSelected() {
+        const selectedRow = document.querySelector('.selected');
+        return selectedRow !== null;
     }
-  });
 
-  table.addEventListener('click', (event) => {
-    event.stopPropagation();
-  });
-
-  minimizeButton.addEventListener('click', () => {
-    sliderWrapper.classList.toggle('minimized');
-    if (sliderWrapper.classList.contains('minimized')) {
-      minimizeButton.textContent = '+';
-      slider.style.display = 'none';
-    } else {
-      minimizeButton.textContent = '-';
-      slider.style.display = 'flex';
+    // Apply minimized effect initially if no row is selected
+    if (!isRowSelected()) {
+        tableContainer.classList.add('minimized');
+        sliderWrapper.classList.add('minimized');
+        minimizeBtn.textContent = '+';
+        minimizeButton.textContent = '+';
+        slider.style.display = 'none';
     }
-  });
-});
 
-document.addEventListener('stationValue', (event) => {
-    const { station } = event.detail;
-    if (station === false) {
-        // Trigger click event on minimize buttons
-        const minimizeBtn = document.querySelector('.history-table-minimize-button');
-        const minimizeButton = document.querySelector('.screenshots-min-button');
-        minimizeBtn.click();
-        minimizeButton.click();
-    }
+    minimizeBtn.addEventListener('click', () => {
+        tableContainer.classList.toggle('minimized');
+        if (tableContainer.classList.contains('minimized')) {
+            minimizeBtn.textContent = '+';
+        } else {
+            minimizeBtn.textContent = '-';
+        }
+    });
+
+    table.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+
+    minimizeButton.addEventListener('click', () => {
+        sliderWrapper.classList.toggle('minimized');
+        if (sliderWrapper.classList.contains('minimized')) {
+            minimizeButton.textContent = '+';
+            slider.style.display = 'none';
+        } else {
+            minimizeButton.textContent = '-';
+            slider.style.display = 'flex';
+        }
+    });
 });
