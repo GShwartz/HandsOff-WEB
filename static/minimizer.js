3f08538a91920c26102bb6e10d7490f53fb12c82
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-16 03:33:32 +0300

    Fixed minimizing bug

--- static/minimizer.js
+++ static/minimizer.js
@@ -1,18 +1,32 @@
-const tableContainer = document.querySelector('.history-table-container');
-const minimizeBtn = document.querySelector('.history-table-minimize-button');
-const table = tableContainer.querySelector('table');
+document.addEventListener('DOMContentLoaded', () => {
+  const tableContainer = document.querySelector('.history-table-container');
+  const minimizeBtn = document.querySelector('.history-table-minimize-button');
+  const table = tableContainer.querySelector('table');
+  const sliderWrapper = document.querySelector('.slider-wrapper');
+  const minimizeButton = sliderWrapper.querySelector('.screenshots-min-button');
+  const slider = sliderWrapper.querySelector('.slider');
 
-minimizeBtn.addEventListener('click', () => {
-  tableContainer.classList.toggle('minimized');
-});
+  minimizeBtn.addEventListener('click', () => {
+    tableContainer.classList.toggle('minimized');
+    if (tableContainer.classList.contains('minimized')) {
+      minimizeBtn.textContent = '+';
+    } else {
+      minimizeBtn.textContent = '-';
+    }
+  });
 
-table.addEventListener('click', (event) => {
-  event.stopPropagation();
-});
+  table.addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
 
-const sliderWrapper = document.querySelector('.slider-wrapper');
-const minimizeButton = document.querySelector('.minimize-button');
-
-minimizeButton.addEventListener('click', () => {
+  minimizeButton.addEventListener('click', () => {
     sliderWrapper.classList.toggle('minimized');
+    if (sliderWrapper.classList.contains('minimized')) {
+      minimizeButton.textContent = '+';
+      slider.style.display = 'none';
+    } else {
+      minimizeButton.textContent = '-';
+      slider.style.display = 'flex';
+    }
+  });
 });
