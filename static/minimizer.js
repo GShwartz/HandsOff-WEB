6eb911945eac6f2f5a2bc71a183ab232a64225c3
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-18 04:15:59 +0300

    Added tasks

--- static/minimizer.js
+++ static/minimizer.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const sliderWrapper = document.querySelector('.screenshots-slider-wrapper');
-  const minimizeSliderButton = sliderWrapper.querySelector('.screenshots-minimize-button');
   const slider = sliderWrapper.querySelector('.screenshots-slider');
+  const minimizeSliderButton = sliderWrapper.querySelector('.screenshots-minimize-button');
 
   const informationWrapper = document.querySelector('.information-wrapper');
-  const minimizeInformationButton = informationWrapper.querySelector('.information-minimize-button');
   const informationContainer = informationWrapper.querySelector('.information-container');
+  const minimizeInformationButton = informationWrapper.querySelector('.information-minimize-button');
+
+  const tasksWrapper = document.querySelector('.tasks-wrapper');
+  const tasksContainer = tasksWrapper.querySelector('.tasks-container');
+  const minimizeTasksButton = tasksWrapper.querySelector('.tasks-minimize-button');
 
   // Initial check for station value
   document.dispatchEvent(new CustomEvent('stationValue', { detail: { station: station } }));
@@ -22,20 +26,26 @@ document.addEventListener('DOMContentLoaded', () => {
       minimizeSliderButton.style.display = 'none';
       minimizeInformationButton.style.display = 'none';
       minimizeInformationButton.textContent = '+';
+      minimizeTasksButton.style.display = 'none';
+      minimizeTasksButton.textContent = '+';
 
     } else {
       minimizeSliderButton.style.display = 'flex';
       minimizeInformationButton.style.display = 'flex';
       minimizeInformationButton.textContent = '-';
+      minimizeTasksButton.style.display = 'flex';
+      minimizeTasksButton.textContent = '-';
     }
 
     // Apply minimized effect initially if no row is selected or station is false
     if (!isRowSelected()) {
       sliderWrapper.classList.add('minimized');
       informationContainer.classList.add('minimized');
+      tasksContainer.classList.add('minimized');
 
       minimizeSliderButton.textContent = '+';
       minimizeInformationButton.textContent = '+';
+      minimizeTasksButton.textContent = '+';
 
       slider.style.display = 'none';
     }
@@ -60,4 +70,14 @@ document.addEventListener('DOMContentLoaded', () => {
     minimizeInformationButton.textContent = '-';
     }
     });
+
+  minimizeTasksButton.addEventListener('click', () => {
+    tasksContainer.classList.toggle('minimized');
+    if (tasksContainer.classList.contains('minimized')) {
+    minimizeTasksButton.textContent = '+';
+    } else {
+    minimizeTasksButton.textContent = '-';
+    }
+    });
+
 });
