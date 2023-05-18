c4361e80b10e61cff81e644ae5b57fe5e7d2c6e9
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-19 02:55:23 +0300

    Re-design
    TODO: show only 1 display container

--- static/controller.js
+++ static/controller.js
@@ -2,22 +2,67 @@ document.addEventListener('DOMContentLoaded', function() {
     const btnsContainer = document.querySelector('.controller-buttons-container');
     btnsContainer.addEventListener('click', handleButtonClick);
 
-    // Handle button clicks
-    function handleButtonClick(event) {
-      const button = event.target.closest('.button');
-      if (!button) return;
+// Final closing
+});
 
-      const action = button.dataset.action;
-      if (action === 'screenshot') {
-        makeAjaxRequest(action);
-        refreshImageSlider();
-      }
+// Handle button clicks
+function handleButtonClick(event) {
+  const button = event.target.closest('.button');
+  if (!button) return;
 
-      else if (action === 'update') {
+  const action = button.dataset.action;
+  if (action === 'screenshot') {
+    makeAjaxRequest(action);
+    refreshImageSlider();
+  }
+
+  else if (action === 'update') {
+    if (!lastSelectedRow) {
+      console.log('No row selected');
+      return;
+    }
+    const overlay = document.createElement('div');
+    const popup = document.createElement('container');
+    popup.classList.add('popup', 'fade-in');
+    setTimeout(() => {
+      popup.classList.remove('visible');
+      void popup.offsetWidth; // Trigger reflow to restart the animation
+      popup.classList.add('visible');
+    }, 50);
+
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    popup.innerHTML = `
+      <h1>Update ${lastSelectedRow.cells[2].innerText}?</h1>
+      <div class="popup-buttons">
+        <button id="yes-button">Yes</button>
+        <button id="no-button">No</button>
+      </div>`;
+
+    document.body.appendChild(popup);
+
+    const yesButton = document.getElementById('yes-button');
+    const noButton = document.getElementById('no-button');
+
+    yesButton.addEventListener('click', handleUpdateConfirmation);
+    noButton.addEventListener('click', () => {
+      popup.remove();
+      overlay.classList.remove('visible');
+      document.body.removeChild(overlay);
+    });
+
+  }
+
+  else if (action === 'sysinfo') {
+        makeAjaxRequest('sysinfo');
+  }
+
+  else if (action === 'restart') {
         if (!lastSelectedRow) {
           console.log('No row selected');
           return;
         }
+
         const overlay = document.createElement('div');
         const popup = document.createElement('container');
         popup.classList.add('popup', 'fade-in');
@@ -30,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
         overlay.classList.add('overlay');
         document.body.appendChild(overlay);
         popup.innerHTML = `
-          <h1>Update ${lastSelectedRow.cells[2].innerText}?</h1>
+          <h1>Restart ${lastSelectedRow.cells[2].innerText}?</h1>
           <div class="popup-buttons">
             <button id="yes-button">Yes</button>
             <button id="no-button">No</button>
@@ -41,206 +86,156 @@ document.addEventListener('DOMContentLoaded', function() {
         const yesButton = document.getElementById('yes-button');
         const noButton = document.getElementById('no-button');
 
-        yesButton.addEventListener('click', handleUpdateConfirmation);
+        yesButton.addEventListener('click', handleRestartConfirmation);
         noButton.addEventListener('click', () => {
           popup.remove();
           overlay.classList.remove('visible');
           document.body.removeChild(overlay);
         });
+  }
 
-      }
-
-      else if (action === 'sysinfo') {
-            makeAjaxRequest('sysinfo');
-      }
-
-      else if (action === 'restart') {
-            if (!lastSelectedRow) {
-              console.log('No row selected');
-              return;
-            }
-
-            const overlay = document.createElement('div');
-            const popup = document.createElement('container');
-            popup.classList.add('popup', 'fade-in');
-            setTimeout(() => {
-              popup.classList.remove('visible');
-              void popup.offsetWidth; // Trigger reflow to restart the animation
-              popup.classList.add('visible');
-            }, 50);
-
-            overlay.classList.add('overlay');
-            document.body.appendChild(overlay);
-            popup.innerHTML = `
-              <h1>Restart ${lastSelectedRow.cells[2].innerText}?</h1>
-              <div class="popup-buttons">
-                <button id="yes-button">Yes</button>
-                <button id="no-button">No</button>
-              </div>`;
-
-            document.body.appendChild(popup);
-
-            const yesButton = document.getElementById('yes-button');
-            const noButton = document.getElementById('no-button');
-
-            yesButton.addEventListener('click', handleRestartConfirmation);
-            noButton.addEventListener('click', () => {
-              popup.remove();
-              overlay.classList.remove('visible');
-              document.body.removeChild(overlay);
-            });
-      }
-
-      else if (action === 'get_tasks') {
-            if (!lastSelectedRow) {
-              console.log('No row selected');
-              return;
-            }
-            makeAjaxRequest('tasks');
-      }
-
-      else {
-          makeAjaxRequest(action);
+  else if (action === 'get_tasks') {
+        if (!lastSelectedRow) {
+          console.log('No row selected');
+          return;
         }
-      }
+        makeAjaxRequest('tasks');
+  }
 
-    function killTask(taskName) {
-      // Send taskName data to server
-      fetch('/kill_task', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 'taskName': taskName })
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Received response from server:', data);
-        })
-        .catch(error => {
-          console.error('Error while sending selected row data to server:', error);
-        });
+  else {
+      makeAjaxRequest(action);
     }
+  }
 
-    function handleRestartConfirmation() {
-      makeAjaxRequest('restart');
-      const popup = document.querySelector('.popup');
+function killTask(taskName) {
+  // Send taskName data to server
+  fetch('/kill_task', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 'taskName': taskName })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Received response from server:', data);
+    })
+    .catch(error => {
+      console.error('Error while sending selected row data to server:', error);
+    });
+}
+
+function handleRestartConfirmation() {
+  makeAjaxRequest('restart');
+  const popup = document.querySelector('.popup');
+  popup.remove();
+  location.reload();
+}
+
+function handleUpdateConfirmation() {
+  makeAjaxRequest('update');
+  const popup = document.querySelector('.popup');
+  popup.remove();
+  location.reload();
+}
+
+function openInstallPopup() {
+  return new Promise((resolve, reject) => {
+    const overlay = document.createElement('div');
+    const popup = document.createElement('container');
+    popup.classList.add('popup', 'fade-in');
+    setTimeout(() => {
+      popup.classList.remove('visible');
+      void popup.offsetWidth; // Trigger reflow to restart the animation
+      popup.classList.add('visible');
+    }, 50);
+
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+
+    popup.innerHTML = `
+      <h1>Install Anydesk on ${lastSelectedRow.cells[2].innerText}</h1>
+      <form>
+        <div class="popup-buttons">
+          <button type="button" id="install-button">Install</button>
+          <button type="button" id="skip-button">Skip</button>
+        </div>
+      </form>
+    `;
+
+    const installButton = popup.querySelector('#install-button');
+    const skipButton = popup.querySelector('#skip-button');
+
+    installButton.addEventListener('click', () => {
+      makeAjaxRequest('install_anydesk');
       popup.remove();
-      location.reload();
-    }
-
-    function handleUpdateConfirmation() {
-      makeAjaxRequest('update');
-      const popup = document.querySelector('.popup');
+      overlay.classList.remove('visible');
+      document.body.removeChild(overlay);
+    });
+    skipButton.addEventListener('click', () => {
+      makeAjaxRequest('skip_anydesk');
       popup.remove();
-      location.reload();
+      overlay.classList.remove('visible');
+      document.body.removeChild(overlay);
+    });
+    document.body.appendChild(popup);
+
+    function closePopup() {
+      overlay.classList.remove('visible');
+      document.body.removeChild(overlay);
     }
+  });
+}
 
-    function openInstallPopup() {
-      return new Promise((resolve, reject) => {
-        const overlay = document.createElement('div');
-        const popup = document.createElement('container');
-        popup.classList.add('popup', 'fade-in');
-        setTimeout(() => {
-          popup.classList.remove('visible');
-          void popup.offsetWidth; // Trigger reflow to restart the animation
-          popup.classList.add('visible');
-        }, 50);
+async function makeAjaxRequest(data) {
+  try {
+    const response = await fetch('/controller', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify({ data })
+    });
 
-        overlay.classList.add('overlay');
-        document.body.appendChild(overlay);
+    const responseData = await response.json();
+    console.log('Received response from Flask backend:', responseData);
 
-        popup.innerHTML = `
-          <h1>Install Anydesk on ${lastSelectedRow.cells[2].innerText}</h1>
-          <form>
-            <div class="popup-buttons">
-              <button type="button" id="install-button">Install</button>
-              <button type="button" id="skip-button">Skip</button>
-            </div>
-          </form>
-        `;
-
-        const installButton = popup.querySelector('#install-button');
-        const skipButton = popup.querySelector('#skip-button');
-
-        installButton.addEventListener('click', () => {
-          makeAjaxRequest('install_anydesk');
-          popup.remove();
-          overlay.classList.remove('visible');
-          document.body.removeChild(overlay);
-        });
-        skipButton.addEventListener('click', () => {
-          makeAjaxRequest('skip_anydesk');
-          popup.remove();
-          overlay.classList.remove('visible');
-          document.body.removeChild(overlay);
-        });
-        document.body.appendChild(popup);
-
-        function closePopup() {
-          overlay.classList.remove('visible');
-          document.body.removeChild(overlay);
-        }
-      });
+    if (responseData.message === 'missing') {
+      openInstallPopup();
     }
-
-    function displaySys() {
+    if (responseData.message === 'skipped') {
+      closePopup();
 
     }
+    if ((responseData.type === 'system') || (responseData.type === 'tasks')) {
+      if (responseData.type === 'system') {
+          var fileName = responseData.fileName;
+          var fileContent = responseData.fileContent;
 
-    async function makeAjaxRequest(data) {
-      try {
-        const response = await fetch('/controller', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json;charset=UTF-8'
-          },
-          body: JSON.stringify({ data })
-        });
+          var informationContainer = document.querySelector('.information-container');
+          var preElement = document.createElement('pre');
+          preElement.textContent = responseData.fileContent;
+          informationContainer.appendChild(preElement);
 
-        const responseData = await response.json();
-        console.log('Received response from Flask backend:', responseData);
+          console.log('responseData', fileName);
+      }
 
-        if (responseData.message === 'missing') {
-          openInstallPopup();
-        }
-        if (responseData.message === 'skipped') {
-          closePopup();
+      else if (responseData.type === 'tasks') {
+        var fileName = responseData.fileName;
+        var fileContent = responseData.fileContent;
 
-        }
-        if ((responseData.type === 'system') || (responseData.type === 'tasks')) {
-          if (responseData.type === 'system') {
-              var fileName = responseData.fileName;
-              var fileContent = responseData.fileContent;
+        var tasksContainer = document.querySelector('.tasks-container');
+        var preElement = document.createElement('pre');
+        preElement.textContent = responseData.fileContent;
+        tasksContainer.appendChild(preElement);
 
-              var informationContainer = document.querySelector('.information-container');
-              var preElement = document.createElement('pre');
-              preElement.textContent = responseData.fileContent;
-              informationContainer.appendChild(preElement);
-
-              console.log('responseData', fileName);
-          }
-
-          else if (responseData.type === 'tasks') {
-            var fileName = responseData.fileName;
-            var fileContent = responseData.fileContent;
-
-            var tasksContainer = document.querySelector('.tasks-container');
-            var preElement = document.createElement('pre');
-            preElement.textContent = responseData.fileContent;
-            tasksContainer.appendChild(preElement);
-
-            console.log('responseData', fileName);
-          }
-        }
-      } catch (error) {
-        console.error('Error in makeAjaxRequest:', error);
+        console.log('responseData', fileName);
       }
     }
-
-// Final closing
-});
-
+  } catch (error) {
+    console.error('Error in makeAjaxRequest:', error);
+  }
+}
 
 
 
--- static/minimizer.js
+++ static/minimizer.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const screenshotHeaderButton = document.getElementById('screenshots-header');
   const sliderWrapper = document.querySelector('.screenshots-slider-wrapper');
   const slider = sliderWrapper.querySelector('.screenshots-slider');
-  const minimizeSliderButton = sliderWrapper.querySelector('.screenshots-minimize-button');
 
+  const informationHeaderButton = document.getElementById('information-header');
   const informationWrapper = document.querySelector('.information-wrapper');
   const informationContainer = informationWrapper.querySelector('.information-container');
-  const minimizeInformationButton = informationWrapper.querySelector('.information-minimize-button');
 
+  const tasksHeaderButton = document.getElementById('tasks-header');
   const tasksWrapper = document.querySelector('.tasks-wrapper');
   const tasksContainer = tasksWrapper.querySelector('.tasks-container');
   const minimizeTasksButton = tasksWrapper.querySelector('.tasks-minimize-button');
@@ -14,27 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
   // Initial check for station value
   document.dispatchEvent(new CustomEvent('stationValue', { detail: { station: station } }));
 
-  // Function to check if any row is selected
-  function isRowSelected() {
-    const selectedRow = document.querySelector('.selected');
-    return selectedRow != null;
-  }
-
   document.addEventListener('stationValue', (event) => {
     const station = event.detail.station;
     if (station === false) {
-      minimizeSliderButton.style.display = 'none';
-      minimizeInformationButton.style.display = 'none';
-      minimizeInformationButton.textContent = '+';
-      minimizeTasksButton.style.display = 'none';
-      minimizeTasksButton.textContent = '+';
+        console.log('station:', station)
 
     } else {
-      minimizeSliderButton.style.display = 'flex';
-      minimizeInformationButton.style.display = 'flex';
-      minimizeInformationButton.textContent = '-';
-      minimizeTasksButton.style.display = 'flex';
-      minimizeTasksButton.textContent = '-';
+        console.log('station:', station)
     }
 
     // Apply minimized effect initially if no row is selected or station is false
@@ -42,42 +29,31 @@ document.addEventListener('DOMContentLoaded', () => {
       sliderWrapper.classList.add('minimized');
       informationContainer.classList.add('minimized');
       tasksContainer.classList.add('minimized');
-
-      minimizeSliderButton.textContent = '+';
-      minimizeInformationButton.textContent = '+';
-      minimizeTasksButton.textContent = '+';
-
       slider.style.display = 'none';
     }
   });
 
-  minimizeSliderButton.addEventListener('click', () => {
+  screenshotHeaderButton.addEventListener('click', () => {
     sliderWrapper.classList.toggle('minimized');
     if (sliderWrapper.classList.contains('minimized')) {
-      minimizeSliderButton.textContent = '+';
       slider.style.display = 'none';
     } else {
-      minimizeSliderButton.textContent = '-';
       slider.style.display = 'flex';
     }
   });
 
-  minimizeInformationButton.addEventListener('click', () => {
+  informationHeaderButton.addEventListener('click', () => {
     informationContainer.classList.toggle('minimized');
-    if (informationContainer.classList.contains('minimized')) {
-    minimizeInformationButton.textContent = '+';
-    } else {
-    minimizeInformationButton.textContent = '-';
-    }
-    });
+  });
 
-  minimizeTasksButton.addEventListener('click', () => {
+  tasksHeaderButton.addEventListener('click', () => {
     tasksContainer.classList.toggle('minimized');
-    if (tasksContainer.classList.contains('minimized')) {
-    minimizeTasksButton.textContent = '+';
-    } else {
-    minimizeTasksButton.textContent = '-';
-    }
-    });
+  });
 
 });
+
+// Function to check if any row is selected
+function isRowSelected() {
+const selectedRow = document.querySelector('.selected');
+return selectedRow != null;
+}
