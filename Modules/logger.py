f22486f01c888fae6301cfb31235726bbe2c5a9a
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-27 22:34:18 +0300

    Add files via upload

--- Modules/logger.py
+++ Modules/logger.py
@@ -6,21 +6,26 @@ import os
 
 def init_logger(log_path, name):
     print(f'log_path: {log_path}')
-    if not log_path:
+    if log_path:
         os.makedirs(os.path.dirname(log_path), exist_ok=True)
 
-    log_queue = queue.Queue()
-    queue_handler = logging.handlers.QueueHandler(log_queue)
+        log_queue = queue.Queue()
+        queue_handler = logging.handlers.QueueHandler(log_queue)
 
-    logger = logging.getLogger(name)
-    if not logger.handlers:
-        logger.setLevel(logging.DEBUG)
-        logger.propagate = False
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        logger = logging.getLogger(name)
+        if not logger.handlers:
+            logger.setLevel(logging.DEBUG)
+            logger.propagate = False
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-        info = logging.FileHandler(log_path)
-        info.setLevel(logging.DEBUG)
-        info.setFormatter(formatter)
-        logger.addHandler(info)
+            if not os.path.isfile(log_path):
+                open(log_path, 'w').close()  # Create an empty log file if it doesn't exist
 
-    return logger
+            info = logging.FileHandler(log_path)
+            info.setLevel(logging.DEBUG)
+            info.setFormatter(formatter)
+            logger.addHandler(info)
+
+        return logger
+    else:
+        raise ValueError("Invalid log path provided.")
