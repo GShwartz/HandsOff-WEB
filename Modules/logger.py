98c8cdd69d0d697192faafdd2ed232e56e9a3a14
Gil Shwartz <58849439+GShwartz@users.noreply.github.com>
2023-05-27 22:06:09 +0300

    Add files via upload

--- Modules/logger.py
+++ Modules/logger.py
@@ -1,12 +1,9 @@
 import logging
 import logging.handlers
-import platform
 import queue
-import os
 
 
 def init_logger(log_path, name):
-    print(log_path)
     log_queue = queue.Queue()
     queue_handler = logging.handlers.QueueHandler(log_queue)
 
@@ -15,19 +12,8 @@ def init_logger(log_path, name):
         logger.setLevel(logging.DEBUG)
         logger.propagate = False
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        try:
-            if platform.system() == 'Windows':
-                # Windows-specific log file path
-                log_path = log_path.replace('/', '\\')
-            info = logging.FileHandler(log_path)
 
-        except FileNotFoundError:
-            with open(log_path, 'w') as file:
-                pass
-
-        except FileExistsError:
-            pass
-
+        info = logging.FileHandler(log_path)
         info.setLevel(logging.DEBUG)
         info.setFormatter(formatter)
         logger.addHandler(info)
